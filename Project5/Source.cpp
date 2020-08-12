96744c21e8b963b0621b51e6196e1dc26b262e91
perth pp <kekskcfm@gmail.com>
2020-08-12 11:02:50 +0700

    Add project files.

---
+++ Project5/Source.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int c;
+	scanf_s("%d", &c);
+	for (int r = 0; r < c; r++)
+	{
+		for (int s = 0; s < r + 1; s++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
